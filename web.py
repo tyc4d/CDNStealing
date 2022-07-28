46269f8a3e57ebf2fc1a2f39e317d79e84aed57b
ed0419 <genden80@gmail.com>
2022-07-28 23:28:27 +0800

    css name

--- web.py
+++ web.py
@@ -24,7 +24,7 @@ def append_log():
 @app.route('/aNa28N')
 def give():
     url = "https://www.voofd.com/"
-    addp = url+str(random.randint(1,100))+str(random.randint(1,100))+str(random.randint(1,100))
+    addp = url+str(random.randint(1,100))+str(random.randint(1,100))+str(random.randint(1,100))+".css"
     app.logger.critical('%s visit successfully', addp)
     print(addp)
     
